5eada58769290188ab4d3694e7bb5d0b80d69e77
Mohamed EL KASSAR <45408469+simomistralii@users.noreply.github.com>
2023-03-06 21:21:18 +0100

    Update demo.js

--- js/demo.js
+++ js/demo.js
@@ -1,6 +1,6 @@
 /**
  * Particleground demo
- * @author Jonathan Nicol - @mrjnicol
+ * @author EOS - Ecosystem
  */
 
 // This can be used to set the Particles Effects. Check README for more details!
@@ -26,3 +26,24 @@ $(document).ready(function() {
   });
 });
 */
+
+
+ var letexte = 'EOS Ecosystem offers a cross-chain NFT marketplace that enables creators to reach a wider audience and collectors to discover and trade unique digital assets.';
+  var montimer;
+  var cmpt = 0;
+  function typewriter(){
+     lelien = document.getElementById('lien');
+     if(cmpt < letexte.length){
+        courant = lelien.innerHTML.substring(0, lelien.innerHTML.length -1);
+        courant += letexte.charAt(cmpt)+" ";
+        lelien.innerHTML = courant;
+     }
+     else{
+        lelien.innerHTML = type;
+     }
+     cmpt++;
+     setTimeout("typewriter()",100);
+  }
+  window.onload = function(){
+     typewriter();
+  }
